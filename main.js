7a460389acf0177c8a2f997a4639b87c52be1bb6
Parish Neighbors <pn8bors@gmail.com>
2022-02-08 13:54:29 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const shopping = [
+    'Apples',
+    'Bannanas',
+    'Pears',
+    'Oranges',
+];
+
+async function main() {
+    console.log('Here is your shopping list');
+    console.log('==========================');
+    shopping.forEach(element => console.log( '-' && element));
+}
+
+main();
